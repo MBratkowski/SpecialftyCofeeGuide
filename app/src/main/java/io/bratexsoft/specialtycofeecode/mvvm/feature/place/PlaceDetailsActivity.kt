56f5c2ea71a734b6df2ec23f934d5e05471d6699
Mateusz <mateusz.bratkowski93@gmail.com>
2018-01-12 14:49:25 +0100

    Created skeleton of the Details Activity

---
+++ app/src/main/java/io/bratexsoft/specialtycofeecode/mvvm/feature/place/PlaceDetailsActivity.kt
@@ -0,0 +1,32 @@
+package io.bratexsoft.specialtycofeecode.mvvm.feature.place
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.bratexsoft.specialtycofeecode.R
+import io.bratexsoft.specialtycofeecode.repository.model.Place
+
+/**
+ * Created by mateuszbratkowski on 12/01/2018.
+ */
+class PlaceDetailsActivity : AppCompatActivity() {
+
+    companion object {
+
+        const val EXTRA_DATA = "EXTRA_DATA"
+
+        fun startActivity(context: Context, place: Place) {
+            val intent = Intent(context, PlaceDetailsActivity::class.java)
+            intent.putExtra(EXTRA_DATA, place)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.place_details_activity)
+    }
+
+   // private fun
+}
