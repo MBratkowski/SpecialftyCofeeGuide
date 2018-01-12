fd287c92f91583eec59959b138b03747a4adf04a
Mateusz <mateusz.bratkowski93@gmail.com>
2018-01-12 14:51:16 +0100

    Update BaseViewHolder constructor with listener

--- app/src/main/java/io/bratexsoft/specialtycofeecode/widget/BaseViewHolder.kt
+++ app/src/main/java/io/bratexsoft/specialtycofeecode/widget/BaseViewHolder.kt
@@ -2,5 +2,6 @@ package io.bratexsoft.specialtycofeecode.widget
 
 import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
+import io.bratexsoft.specialtycofeecode.mvvm.feature.places.item.ItemClickListener
 
-class BaseViewHolder<out Binding : ViewDataBinding>(val binding: Binding) : RecyclerView.ViewHolder(binding.root)
+class BaseViewHolder<out Binding : ViewDataBinding>(val binding: Binding, val listener: ItemClickListener) : RecyclerView.ViewHolder(binding.root)
