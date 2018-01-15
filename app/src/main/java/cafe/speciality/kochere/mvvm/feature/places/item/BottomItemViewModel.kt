999f6a0596e2fa40b70a5a97d8f1755b12ec90c2
Mateusz <mateusz.bratkowski93@gmail.com>
2018-01-15 23:08:08 +0100

    Change concept of the application architecture

--- app/src/main/java/cafe/speciality/kochere/mvvm/feature/places/item/BottomItemViewModel.kt
+++
@@ -1,9 +0,0 @@
-package cafe.speciality.kochere.mvvm.feature.places.item
-
-/**
- * Created by mateuszbratkowski on 10/01/2018.
- */
-class
-BottomItemViewModel : PlacesType() {
-    override fun getViewType(): Int = ITEM_BOTTOM
-}
