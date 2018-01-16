ed35682a13abe76f01f85d1e5e6cc3853493bcf4
Mateusz <mateusz.bratkowski93@gmail.com>
2018-01-16 16:34:08 +0100

    Update DI package

--- app/src/main/java/cafe/speciality/kochere/di/component/ActivityComponent.kt
+++ app/src/main/java/cafe/speciality/kochere/di/component/ActivityComponent.kt
@@ -1,11 +1,11 @@
 package cafe.speciality.kochere.di.component
 
 import android.content.Context
-import dagger.Component
 import cafe.speciality.kochere.di.component.activity.PlacesSubcomponent
 import cafe.speciality.kochere.di.module.activity.ActivityModule
 import cafe.speciality.kochere.di.module.activity.PlacesModule
 import cafe.speciality.kochere.di.scope.ActivityScope
+import dagger.Component
 
 @ActivityScope
 @Component(dependencies = [(ApplicationComponent::class)], modules = [(ActivityModule::class)])
--- app/src/main/java/cafe/speciality/kochere/di/component/ApplicationComponent.kt
+++ app/src/main/java/cafe/speciality/kochere/di/component/ApplicationComponent.kt
@@ -1,10 +1,11 @@
 package cafe.speciality.kochere.di.component
 
-import dagger.Component
 import cafe.speciality.kochere.applicaton.SpecialtyCoffeGuideApp
 import cafe.speciality.kochere.di.module.ApplicationModule
 import cafe.speciality.kochere.di.module.NetworkModule
+import cafe.speciality.kochere.domain.UseCaseFactory
 import cafe.speciality.kochere.repository.network.RequestDefinition
+import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
@@ -13,4 +14,6 @@ interface ApplicationComponent {
     fun inject(application: SpecialtyCoffeGuideApp)
 
     fun provideRequestDefinition(): RequestDefinition
+
+    fun provideUseCaseFactory(): UseCaseFactory
 }
