479d6893725df4f40db271ed5a417f8b0212c840
Mateusz <mateusz.bratkowski93@gmail.com>
2018-01-16 16:34:51 +0100

    Deleted BaseViewHolder

--- app/src/main/java/cafe/speciality/kochere/widget/BaseViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package cafe.speciality.kochere.widget
-
-import android.databinding.ViewDataBinding
-import android.support.v7.widget.RecyclerView
-
-class BaseViewHolder<out Binding : ViewDataBinding>(val binding: Binding) : RecyclerView.ViewHolder(binding.root)
