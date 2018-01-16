aca1b3f220b85eef317b7cffcf1070f97dd483cb
Mateusz <mateusz.bratkowski93@gmail.com>
2018-01-16 16:34:36 +0100

    Create DataCallback interface

--- app/src/main/java/cafe/speciality/kochere/repository/remote/DataCallback.kt
+++ app/src/main/java/cafe/speciality/kochere/repository/remote/DataCallback.kt
@@ -3,7 +3,7 @@ package cafe.speciality.kochere.repository.remote
 /**
  * Created by mateuszbratkowski on 15/01/2018.
  */
-interface DataCallback<T> {
+interface DataCallback<in T> {
     fun onSuccess(data: T)
 
     fun onFailure(message: String)
